7badb5fb096fe64d81ed8036ae5f05812837f8f5
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-02-07 09:59:58 +0100

    ranking data

--- src/App.js
+++ src/App.js
@@ -6,29 +6,38 @@ import Data from "../src/testData.json";
 function App() {
   return (
     <div>
-     <header className="head">
-        <img src={melon} alt="a slice of watermelon with blue seeds" className="melonSeed"></img>
-    </header>
+      <header className="head">
+          <img src={melon} alt="a slice of watermelon with blue seeds" className="melonSeed"></img>
+      </header>
     <>
         <div className="sideBar"></div>
+        
         <div className="containerBox">
-              { Data && Data.map (data => {
+              { Data && Data
+              .sort((a, b) => a.marketId > b.marketId ? 1 : -1)
+              .map (data => {
                 return (
+
                   <div className="boxPink" key={ data.id }>
                     
                     {
                       data.testData && data.testData.map( data => {
                         return (
-                         <div key={ data.id }> { data.sport.sportName } { data.market.marketId }</div>
+                        <div key={ data.id }> 
+                          { data.sport.sportName } || 
+                          Event I.D:{ data.sport.eventId } ||
+                          Market I.D:{ data.market.marketId }
+                        </div>
                         )
                       })
                     }
+                    
                   </div>
                 )
               })
             
               }
-            </div>
+        </div>
     </>
     </div>
   );
@@ -37,3 +46,9 @@ function App() {
 export default App;
 
 
+/*
+data.sort((a, b) => {
+  if (a.market.marketId < b.market.marketId)
+  { return -1 ;}
+});
+*/
